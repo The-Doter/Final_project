2276249e05ef623f0578d390614862b19506baad
The-Doter <kleonn7632@gmail.com>
2024-05-08 09:07:57 +0800

    Final_project

---
+++ Assets/Scenes/Menu/GameManager.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameManager : MonoBehaviour
+{
+    public GameObject Pause;
+
+    public void Start()
+    {
+        Pause.SetActive(false);
+    }
+
+    public void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.Escape))
+        {
+            Pause.SetActive(true);
+        }
+    }
+
+    public void QuitToMenu()
+    {
+        SceneManager.LoadScene("Alina");
+    }
+
+    public void Quit()
+    {
+        Application.Quit();
+    }
+
+    public void StartGame()
+    {
+        SceneManager.LoadScene("GameplayScene");
+    }
+}
